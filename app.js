a962835599ebb607a5fb1527352cf6379b61000f
omsharma2364 <omsharmadot2364@gmail.com>
2016-04-22 22:43:40 +0530

    changed app.js, moved express part to config file

--- app.js
+++ app.js
@@ -1,42 +1,22 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var session=require('express-session');
-var bodyParser = require('body-parser');
 var mongoose=require('mongoose');
   // mongoose.set('debug', true);
-
-var multipart = require('connect-multiparty');
-
 var CONF_FILE=require('./conf.json');
-//////////////////////////////////////////////////////////
-//database 
+//Database
 //local
-// add this to git ignore conf.json
 var db=mongoose.connect('mongodb://127.0.0.1/VEND');
 // online
 // var db=mongoose.connect('mongodb://'+CONF_FILE.MONGO_DB.USERNAME+':'+CONF_FILE.MONGO_DB.PASSWORD+'@'+CONF_FILE.MONGO_DB.ADDRESS);
 
-/////////////////////////////////////////////////////////
+var session=require('express-session');
 var MongoStore=require('connect-mongo')(session);
 var routes = require('./routes/index');
 var apiRoutes = require('./api/apiRoutes');
 var app = express();
 var port=process.env.PORT||3000;
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(express.static(__dirname + '/public'));         // set the static files location   
-
-// uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-
-app.use(cookieParser());
+require('./config')(app);
+//session setup
 app.use(session({
     secret:'anyrandomstring',
     store: new MongoStore({
@@ -45,20 +25,10 @@ app.use(session({
     resave: false,
     saveUninitialized: true
 }));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true,limit: '50mb' }));
-app.use(multipart());
-app.engine('html', require('ejs').renderFile);
 
-app.use(function(req,res,next){
-  res.set('X-Powered-By','VEND');
-  next();
-});
-//app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/api',apiRoutes);
-
 
 //constants
 app.locals=require('./views/titles.json');
---
+++ config.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var session=require('express-session');
+var bodyParser = require('body-parser');
+var multipart = require('connect-multiparty');
+
+module.exports=function(app){
+	// express setup setup
+	app.set('views', path.join(__dirname, 'views'));
+	app.set('view engine', 'ejs');
+	app.use(express.static(__dirname + '/public'));         // set the static files location   
+	app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+	app.use(logger('dev'));
+	app.use(cookieParser());
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({ extended: true,limit: '50mb' }));
+	app.use(multipart());
+	app.engine('html', require('ejs').renderFile);
+	app.use(function(req,res,next){
+	  res.set('X-Powered-By','VEND');
+	  next();
+	});
+}
